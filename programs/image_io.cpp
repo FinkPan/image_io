0b9a60dd7d23f7fc433bc31258880a44c7bb8e88
fink <hspan1234@gmail.com>
2013-06-09 16:31:10 +0800

    P1~P3 done.

--- programs/image_io.cpp
+++ programs/image_io.cpp
@@ -20,26 +20,37 @@ static void Usage()		//Usage
 void pmb::readPMBFile(const string &filename)
 {
 	ifstream pmbFile;
-	int ndata;
-	char ccomment;
+	int ndata;	////因为不知道vector怎么处理流,便设了个中间变量用来push_back();
+	char c_is_comment;	//判断第一个字母是否为'#'.如果是'#',则说明有注释行.
+	int nWidthLocal;	//P3时 nWidthLocal为nWidth的3倍.
+	string str_is_comment;	////写入注释
 	pmbFile.open(filename);
 	if(pmbFile.is_open())
 	{
-
-		pmbFile >> strFileForm;
-		pmbFile >> ccomment;	//取出第二行第一个字符,如果是'#',则说明有注释行.
-		if (ccomment == '#')
+		pmbFile >> strFileFormat;	//文件第一行为文件格式.
+		pmbFile >> c_is_comment;	//第二行开始取第一个字符判断,如果是'#',则说明有注释行.
+		while(c_is_comment == '#')
 		{
 			pmbFile.unget();	//Unget character
-			getline(pmbFile,strComment);	//写入注释
+			getline(pmbFile,str_is_comment);	//写入注释
+			strComment.push_back(str_is_comment);
+			pmbFile >> c_is_comment;	//设置while增量
 		}
-		else
-			pmbFile.unget();	//没有注释行,忽略读取,以便后面从新读取.
-		pmbFile >> nColumn >> nRow;		//读取 列,行
-		for (int j = 0; j != nRow; ++j)	//处理行
+		pmbFile.unget();	//没有注释行,忽略读取,以便后面从新读取.
+		pmbFile >> nWidth >> nHeigth;		//读取 列,行]
+		nWidthLocal = nWidth;
+		if ("P2" == strFileFormat)
 		{
-			
-			for (int i = 0; i != nColumn; ++i)	//处理列
+			pmbFile >> nMaxColor;
+		}
+		if ("P3" == strFileFormat)
+		{
+			pmbFile >> nMaxColor;
+			nWidthLocal *= 3;		//P3时 nWidth为3倍.
+		}
+		for (int j = 0; j != nHeigth; ++j)	//处理行
+		{
+			for (int i = 0; i != nWidthLocal; ++i)	//处理列
 			{
 				pmbFile >> ndata;		//因为不知道vector怎么处理流,便设了个中间变量用来push_back();
 				vecRow.push_back(ndata);		
@@ -47,35 +58,54 @@ void pmb::readPMBFile(const string &filename)
 			vecColumn.push_back(vecRow);
 			vecRow.clear();	//清空每一行数据，以便处理新一行数据
 		}
-
 	}
 	else
 		cout << "can't open input file: " << filename << endl;	//读取文件失败提示用户信息.
 	pmbFile.close();	//关闭文件
 	cout << "Finsh reading input file." << endl;	//读取文件成果提示用户信息.
-
 }
 
 void pmb::writePMBFile(const string &filename)
 {
 	ofstream writePMB;
+	int nwidth = 1;
+
 	writePMB.open(filename);
-	
 	if (writePMB.is_open())
 	{
-		writePMB << strFileForm << endl;	//写入文件格式
-		if (strComment != "") //如果注释为不为空,写入注释.
+		writePMB << strFileFormat << endl;	//写入文件格式
+		if (strFileFormat != "") //如果注释为不为空,写入注释.
 		{
-			writePMB << strComment << endl;	
+			for (vector<string>::iterator iterComment = strComment.begin(); iterComment != strComment.end(); ++iterComment)
+			{
+				writePMB << *iterComment << endl;
+			}
+				
 		}
-		writePMB << nColumn << " " << nRow;		//写入行列矩阵
+		writePMB << nWidth << " " << nHeigth;		//写入行列矩阵
+
+		if ("P2" == strFileFormat)		//P3格式 输出宽度为2
+		{
+			writePMB << endl;
+			writePMB << nMaxColor;
+			nwidth = 2;
+		}
+		if ("P3" == strFileFormat)		//P3格式 输出宽度为3
+		{
+			writePMB << endl;
+			writePMB << nMaxColor;
+			nwidth = 3;
+		}
+
 		for (vector<vector<int>>::iterator iterRow = vecColumn.begin(); iterRow != vecColumn.end(); ++iterRow)	//处理行
 		{
 			vecRow = *iterRow;	//将行分离出来
 			writePMB << endl;	//先换行再输出，这样可以避免文件最后有空行。
 			for (vector<int>::iterator iterColunm = vecRow.begin(); iterColunm != vecRow.end(); ++iterColunm)	//处理列
 			{
-				writePMB << *iterColunm << " ";		//写入行
+				writePMB.setf(ios::left);
+				writePMB.width(nwidth);		//设置输出格式宽度
+				writePMB << *iterColunm << " ";		//写入行	
 			}			
 		}
 	}
--- programs/image_io.h
+++ programs/image_io.h
@@ -4,19 +4,23 @@
 #include <string>
 #include <vector>
 using namespace std;
+
 class pmb
 {
 public:
 	void readPMBFile(const string &filename);
 	void writePMBFile(const string &filename);
-	pmb(){strFileForm = "";strComment = ""; nColumn = 0; nRow = 0;}	//默认构造函数,对其理解不是很好.
+	pmb(){strFileFormat = ""; nWidth = 0; nHeigth = 0;}	//默认构造函数,对其理解不是很好.
 private:
-	string strFileForm;
-	string strComment;
-	int nColumn;
-	int nRow;
+	string strFileFormat;
+	vector<string> strComment;
+	int nWidth;
+	int nHeigth;
+	int nMaxColor;
 	vector<int> vecRow;
 	vector<vector<int>> vecColumn;
 };
+
+
 
 #endif
