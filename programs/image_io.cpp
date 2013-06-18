5af56532c576b73587338b1fdfb9b5994b0f4398
fink <hspan1234@gmail.com>
2013-06-18 09:50:11 +0800

    P1~P6完成

--- programs/image_io.cpp
+++ programs/image_io.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <cstring>
 
 #include "image_io.h"
 using namespace std;
@@ -12,114 +13,326 @@ using namespace std;
 static void Usage()		//Usage
 {
 	printf(
-		"Usage: image_io [-in xmlfile] [-ou outfile]\n"
+		"Usage: image_io [-in inputfile] [-ou [-b] outfile]\n"
+		"-in : inputfile.\n"
+		"-ou : outfile\n"
+		"-b : write outfile by binary."
 		);
 }
 
-
-void pmb::readPMBFile(const string &filename)
+void imageIO::ReadimageIOFile(const string &filename)
 {
-	ifstream pmbFile;
-	int ndata;	////因为不知道vector怎么处理流,便设了个中间变量用来push_back();
-	char c_is_comment;	//判断第一个字母是否为'#'.如果是'#',则说明有注释行.
-	int nWidthLocal;	//P3时 nWidthLocal为nWidth的3倍.
-	string str_is_comment;	////写入注释
-	pmbFile.open(filename);
-	if(pmbFile.is_open())
+	ifstream filestr;
+	int filelength;		//文件长度
+	filestr.open(filename,ios_base::in | ios_base::binary);
+	if (filestr)
 	{
-		pmbFile >> strFileFormat;	//文件第一行为文件格式.
-		pmbFile >> c_is_comment;	//第二行开始取第一个字符判断,如果是'#',则说明有注释行.
-		while(c_is_comment == '#')
+		//get filelength of file:
+		filestr.seekg (0, ios::end);
+		filelength = filestr.tellg();
+		filestr.seekg (0, ios::beg);
+
+		//将数据一次性写入str_image_data
+		str_image_data.resize(filelength);
+		filestr.read (&str_image_data[0],filelength);
+
+		size_t pose_not_letter_or_number = 0;	//指向非字母,非数字
+		size_t pose_is_letter_or_number = 0;		//指向字母或者数字
+
+		/******************处理文件格式***********************/
+		str_file_format.resize(2);							//文件格式固定为P1~P6	即2个字节
+		str_file_format = str_image_data.substr(0,2);		
+		/******************处理文件格式***********************/
+
+
+			/******************处理注释***********************/
+		char cdata;			//检查第一个字符是否为'#',用来判断是否为注释
+		int nCommentstart;	//标记注释起始位置
+		pose_not_letter_or_number = str_image_data.find_first_of(" \r\n");									//寻找第一个回车符位置
+		pose_is_letter_or_number = str_image_data.find_first_not_of(" \r\n",pose_not_letter_or_number);		//从第一个回车符开始寻找第一个不是回车符的位置
+		nCommentstart = pose_is_letter_or_number;	
+		cdata = str_image_data[pose_is_letter_or_number];
+		while (cdata == '#')	
 		{
-			pmbFile.unget();	//Unget character
-			getline(pmbFile,str_is_comment);	//写入注释
-			strComment.push_back(str_is_comment);
-			pmbFile >> c_is_comment;	//设置while增量
+			pose_not_letter_or_number = str_image_data.find_first_of("\r\n",pose_is_letter_or_number);	//仅查找\r\n,忽略空格
+			pose_is_letter_or_number = str_image_data.find_first_not_of(" \r\n",pose_not_letter_or_number); 
+			cdata = str_image_data[pose_is_letter_or_number];
+			str_comment = str_image_data.substr(nCommentstart, pose_not_letter_or_number - nCommentstart);
+			nCommentstart = pose_is_letter_or_number;
+			vec_comment.push_back(str_comment);
 		}
-		pmbFile.unget();	//没有注释行,忽略读取,以便后面从新读取.
-		pmbFile >> nWidth >> nHeigth;		//读取 列,行]
-		nWidthLocal = nWidth;
-		if ("P2" == strFileFormat)
+		/******************处理注释***********************/
+
+		/******************处理width***********************/
+		pose_not_letter_or_number = str_image_data.find_first_of(" \r\n",pose_is_letter_or_number);
+		str_width = str_image_data.substr(pose_is_letter_or_number, pose_not_letter_or_number - pose_is_letter_or_number);	
+		nwidth = stoi(str_width);
+		/******************处理width***********************/
+
+		/******************处理Height***********************/
+		pose_is_letter_or_number = str_image_data.find_first_not_of(" \r\n",pose_not_letter_or_number);
+		pose_not_letter_or_number = str_image_data.find_first_of(" \r\n",pose_is_letter_or_number);
+		str_height = str_image_data.substr(pose_is_letter_or_number, pose_not_letter_or_number - pose_is_letter_or_number);
+		nheight = stoi(str_height);
+		/******************处理Height***********************/
+
+		if (str_file_format != "P1" && str_file_format != "P4")	//P1, P4 没有最大值
 		{
-			pmbFile >> nMaxColor;
+			/******************处理str_max_value***********************/
+			pose_is_letter_or_number = str_image_data.find_first_not_of(" \r\n",pose_not_letter_or_number);
+			pose_not_letter_or_number = str_image_data.find_first_of(" \r\n",pose_is_letter_or_number);
+			str_max_value = str_image_data.substr(pose_is_letter_or_number, pose_not_letter_or_number - pose_is_letter_or_number);
+			nmax_value = stoi(str_max_value);
+			/******************处理str_max_value***********************/
 		}
-		if ("P3" == strFileFormat)
-		{
-			pmbFile >> nMaxColor;
-			nWidthLocal *= 3;		//P3时 nWidth为3倍.
-		}
-		for (int j = 0; j != nHeigth; ++j)	//处理行
-		{
-			for (int i = 0; i != nWidthLocal; ++i)	//处理列
-			{
-				pmbFile >> ndata;		//因为不知道vector怎么处理流,便设了个中间变量用来push_back();
-				vecRow.push_back(ndata);		
-			} //一行处理完
-			vecColumn.push_back(vecRow);
-			vecRow.clear();	//清空每一行数据，以便处理新一行数据
-		}
+
+		/******************处理matrix***********************/
+		pose_is_letter_or_number = str_image_data.find_first_not_of(" \r\n",pose_not_letter_or_number);
+		pose_not_letter_or_number = str_image_data.find_first_of(" \r\n",pose_is_letter_or_number);
+		str_matrix = str_image_data.substr(pose_is_letter_or_number, std::string::npos - pose_is_letter_or_number);	//将剩下的数据全部放到str_matrix
+		/******************处理matrix***********************/
+
+		if (str_file_format == "P1" || str_file_format == "P2" ||str_file_format == "P3" )	//P1,P2,P3格式写入vec_text_matrix
+			TextStrMatrixToVecTextMatrix(str_matrix);
+
+		if (str_file_format == "P4" || str_file_format == "P5" || str_file_format == "P6")	//P4,P5,P6格式写入vec_binary_matrix
+			BinaryStrMatrixToVecBinaryMatrix(str_matrix);
+
+		std::cout << "Finsh read the file." << endl;
 	}
 	else
-		cout << "can't open input file: " << filename << endl;	//读取文件失败提示用户信息.
-	pmbFile.close();	//关闭文件
-	cout << "Finsh reading input file." << endl;	//读取文件成果提示用户信息.
+		std::cout << "无法打开: " << filename << endl;
+
 }
 
-void pmb::writePMBFile(const string &filename)
+void imageIO::WriteimageIOFile(const string &filename,bool bBinary)
 {
-	ofstream writePMB;
-	int nwidth = 1;
-
-	writePMB.open(filename);
-	if (writePMB.is_open())
+	ofstream ofilestr;
+	int noutput_width;
+	ofilestr.open(filename,ios_base::out | ios_base::binary);
+	if (ofilestr)
 	{
-		writePMB << strFileFormat << endl;	//写入文件格式
-		if (strFileFormat != "") //如果注释为不为空,写入注释.
+		string str_output_fileformat = "";
+		int noutput_width = nwidth;
+
+		if (!bBinary)	//输出为文本格式
 		{
-			for (vector<string>::iterator iterComment = strComment.begin(); iterComment != strComment.end(); ++iterComment)
+			//输出文件格式
+			if (str_file_format == "P4")	//如果二进制输出文本文件,对应文件格式需要改变.并转换相应的Matrix
 			{
-				writePMB << *iterComment << endl;
+				str_output_fileformat = "P1";
+				VecBinaryMatrixToVecTextMatrix();
 			}
-				
-		}
-		writePMB << nWidth << " " << nHeigth;		//写入行列矩阵
-
-		if ("P2" == strFileFormat)		//P3格式 输出宽度为2
-		{
-			writePMB << endl;
-			writePMB << nMaxColor;
-			nwidth = 2;
-		}
-		if ("P3" == strFileFormat)		//P3格式 输出宽度为3
-		{
-			writePMB << endl;
-			writePMB << nMaxColor;
-			nwidth = 3;
-		}
-
-		for (vector<vector<int>>::iterator iterRow = vecColumn.begin(); iterRow != vecColumn.end(); ++iterRow)	//处理行
-		{
-			vecRow = *iterRow;	//将行分离出来
-			writePMB << endl;	//先换行再输出，这样可以避免文件最后有空行。
-			for (vector<int>::iterator iterColunm = vecRow.begin(); iterColunm != vecRow.end(); ++iterColunm)	//处理列
+			else if (str_file_format == "P5")	
 			{
-				writePMB.setf(ios::left);
-				writePMB.width(nwidth);		//设置输出格式宽度
-				writePMB << *iterColunm << " ";		//写入行	
-			}			
+				str_output_fileformat = "P2";
+				VecBinaryMatrixToVecTextMatrix();
+			}
+			else if (str_file_format == "P6")	
+			{
+				str_output_fileformat = "P3";
+				VecBinaryMatrixToVecTextMatrix();
+			}
+			else
+				str_output_fileformat = str_file_format;
+
+			ofilestr << str_output_fileformat << endl; 
+
+			//输出注释,注释可有可无
+			if (!vec_comment.empty())
+				for (vector<string>::iterator vec_str_iter = vec_comment.begin(); vec_str_iter != vec_comment.end(); ++vec_str_iter)
+					ofilestr << *vec_str_iter << endl;
+
+			//输出宽度和高度
+			ofilestr << str_width << " " << str_height << endl;
+
+			//输出最大值,P1,P4格式没有最大值.
+			if (!str_max_value.empty())
+				ofilestr << str_max_value << endl; 
+
+			//输出Matrix
+			for (int i = 0; i != vec_text_matrix.size(); ++i)
+			{
+				ofilestr.width(3);	//文本文件设置宽度为3,对齐数字比较整齐.
+				ofilestr << vec_text_matrix[i] << " ";
+				int nenter = i +1;	
+				if (str_output_fileformat == "P3")		//P3格式3个宽度单位
+					noutput_width = nwidth*3;
+				if (nenter % noutput_width == 0)		//为了格式对齐.输出回车行
+					ofilestr << endl;
+			}
 		}
+		if (bBinary)	//输出为二进制格式
+		{
+
+			if (str_file_format == "P1")		//如果文本文件输出二进制文件,对应文件格式需要改变,并转换相应的Matrix
+			{
+				str_output_fileformat = "P4";
+				VecTextMatrixToVecBinaryMatrix();
+			}
+			else if (str_file_format == "P2")
+			{
+				str_output_fileformat = "P5";
+				VecTextMatrixToVecBinaryMatrix();
+			}
+			else if (str_file_format == "P3")
+			{
+				str_output_fileformat = "P6";
+				VecTextMatrixToVecBinaryMatrix();
+			}
+			else
+				str_output_fileformat = str_file_format;
+
+			//输出文件格式
+			ofilestr << str_output_fileformat << endl;	
+
+			//输出注释,注释可有可无
+			if (!vec_comment.empty())
+				for (vector<string>::iterator vec_str_iter = vec_comment.begin(); vec_str_iter != vec_comment.end(); ++vec_str_iter)
+					ofilestr << *vec_str_iter << endl;
+
+			//输出宽度和高度
+			ofilestr << str_width << " " << str_height << endl;
+
+			if (str_output_fileformat != "P4")	//二进制输出P4没有最大值,其他最大值为255 (该值参考PS所设)
+				ofilestr << "255" << endl; 
+
+			for (int i = 0; i != vec_binary_matrix.size(); ++i)
+				ofilestr.write((char*)&vec_binary_matrix[i],1);
+		}
+		ofilestr.close();
+		std::cout << "Finish write the file:" << filename  << endl;
 	}
 	else
-		cout << "can't open the output file: " << filename  << endl;	//写入文件失败提示用户信息.
-	writePMB.close();	//关闭文件.
-	cout << "finsh write pmb file.\n" ;	//写入文件成果提示用户信息.
+		std::cout << "Can't not open the output file: " << filename << endl;
+}
+
+//将二进制Matrix转为vec_binary_matrix
+void imageIO::BinaryStrMatrixToVecBinaryMatrix(const string &strSequence)
+{
+	size_t nsize = strSequence.size();
+	vec_binary_matrix.resize(nsize);
+	for (int i = 0; i < nsize; ++i)
+		vec_binary_matrix[i] = strSequence[i];
+}
+
+//将文本Matrix转为vec_text_matrix
+void imageIO::TextStrMatrixToVecTextMatrix(const string &strSequence)
+{
+	size_t pose_not_letter_or_number = 0;
+	size_t pose_is_letter_or_number = 0;
+	int nMatrix;
+	size_t nsize = strSequence.size();
+	pose_not_letter_or_number = strSequence.find_first_not_of("0123456789");
+	string strtemp;
+	while(string::npos != pose_is_letter_or_number)
+	{
+		strtemp = strSequence.substr(pose_is_letter_or_number,pose_not_letter_or_number-pose_is_letter_or_number);
+		nMatrix = stoi(strtemp);
+		vec_text_matrix.push_back(nMatrix);
+		pose_is_letter_or_number = strSequence.find_first_of("0123456789",pose_not_letter_or_number);
+		pose_not_letter_or_number = strSequence.find_first_not_of("0123456789",pose_is_letter_or_number);
+	}
+}
+
+//将VecBinaryMatrix转为ToVecTextMatrix
+void imageIO::VecBinaryMatrixToVecTextMatrix()
+{
+	size_t nsize = vec_binary_matrix.size();
+	if (str_file_format == "P4")	//P4文件格式: 一个像素占1位,8个像素占1个字节.
+	{
+		int nbitwidth;	//行标记,如果行nbitwidth大于nwidth,忽略后面行;
+		//vec_text_matrix.resize(nwidth*nheight);
+		for (int i = 0; i < nsize; ++i)
+		{
+			nbitwidth = 0;	//重置行
+			for (int j = 0; j < 8; j++)
+			{
+				//将一个字节每一位和vec_binary_matrix[i] 与 运算,如果为真,vec_text_matrix[i],写入1,否则写入0
+				if((nbitwidth < nwidth) && vec_binary_matrix[i] & (0x80 >> j))		
+				{
+					vec_text_matrix.push_back(1);
+					++nbitwidth;
+				}
+				else if((nbitwidth < nwidth))
+				{
+					vec_text_matrix.push_back(0);
+					++nbitwidth;
+				}
+				else	//行nbitwidth大于nwidth,忽略后面行;
+					break;	
+			}
+		}
+	}
+	else	//P5,P6文件格式: 一个像素占1个字节
+	{
+		vec_text_matrix.resize(nsize);
+		int ndec,nhex;
+		int nmark = 0x00ff;	//屏蔽高8位数
+		for (int i = 0; i < nsize; ++i)
+		{
+			nhex = vec_binary_matrix[i];
+			ndec = nhex & nmark;	//屏蔽高8位数
+			vec_text_matrix[i] = ndec;
+		}
+	}
+}
+
+//将VecTextMatrix转为VecBinaryMatrix
+void imageIO::VecTextMatrixToVecBinaryMatrix()
+{
+	size_t nsize = vec_text_matrix.size();
+	if (str_file_format == "P1") //P1文件格式: 一个像素占1位,8个像素占1个字.
+	{
+		for (int i = 0; i != nsize; )
+		{
+			int nbit = 0;	//1个字节有8位,位标记,每8位就需要重置.
+			char cz = 0x00;
+  			for (int k = 0; k != nwidth; ++k)
+  			{
+				if (nbit == 8)	//完成一个字节,写入vec_binary_matrix,并更新 位标记
+				{
+					vec_binary_matrix.push_back(cz);
+					cz = 0x00;
+					nbit =0;
+				}
+				if (vec_text_matrix[i] == 1)	
+					cz = cz | (0x80>>nbit);		//如果数值为1则进行 或 运算,将8位用一个字节表示.
+				++nbit;
+				++i;
+			}
+			vec_binary_matrix.push_back(cz);	//写入未满8位的字节,并更新 位标记
+			cz = 0x00;
+		}
+	}
+	else	//P2,文件格式: 一个像素占1个字节
+	{
+		int nbinary_value;
+		int ndecimal_value;
+		float stemp;
+		for (int i = 0; i != nsize; ++i)
+		{
+			ndecimal_value = vec_text_matrix[i];
+
+			//按照比例最大值比例将文本文件值转换为最大值为255对应的值
+			stemp =  float(ndecimal_value * 255) /nmax_value;		
+			if (abs(stemp - ((int)stemp)) == 0)		//这样上取值是否可行?或者有没有更规范的上取值的方法?
+				nbinary_value = stemp;
+			else
+				nbinary_value = stemp + 1;
+
+			vec_binary_matrix.push_back(nbinary_value);	//将值写入vec_binary_matrix
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
-	pmb mypbm;
+	imageIO mypbm;
 	const char* pszInFile = NULL;
 	const char* pszOutFile = NULL;
+	bool bBinaryFile = false;
+
 	if(argc == 1)
 		Usage();
 
@@ -131,7 +344,14 @@ int main(int argc, char **argv)
 		}
 		else if (EQUAL(argv[i],"-ou"))
 		{
-			pszOutFile = argv[++i];
+			if ((EQUAL(argv[++i],"-b")))
+			{
+				bBinaryFile = true;
+				pszOutFile = argv[++i];
+			}
+			else
+				pszOutFile = argv[i];
+	
 		}
 		else
 		{
@@ -141,10 +361,11 @@ int main(int argc, char **argv)
 	}
 	if (!pszInFile == NULL && !pszOutFile == NULL)
 	{
-		mypbm.readPMBFile(pszInFile);
-		mypbm.writePMBFile(pszOutFile);
+		mypbm.ReadimageIOFile(pszInFile);
+		mypbm.WriteimageIOFile(pszOutFile,bBinaryFile);
 	}
+	else
+		std::cout << "输入参数有错." << endl;
 
-	system("PAUSE");
 	return 0;
 }
--- programs/image_io.h
+++ programs/image_io.h
@@ -5,21 +5,50 @@
 #include <vector>
 using namespace std;
 
-class pmb
+class imageIO
 {
 public:
-	void readPMBFile(const string &filename);
-	void writePMBFile(const string &filename);
-	pmb()
-		: strFileFormat(""), nWidth(0), nHeigth(0) {}	//默认类构造函数
+	void ReadimageIOFile(const string &filename);
+	//void WriteimageIOFile(const string &filename);
+	void WriteimageIOFile(const string &filename,bool bBinary);
+
+	//P1需要转为vector<bool>时可用. (暂时没有用)
+	void BinaryStringToVectorBool(const string &strSequence, vector<bool> &vecbSequence);
+	
+	//将二进制Matrix转为VecBinaryMatrix
+	void BinaryStrMatrixToVecBinaryMatrix(const string &strSequence);
+	
+	//将文本Matrix转为VecTextMatrix
+	void TextStrMatrixToVecTextMatrix(const string &strSequence);	
+
+	//将VecBinaryMatrix转为ToVecTextMatrix
+	void VecBinaryMatrixToVecTextMatrix();
+
+	//将VecTextMatrix转为VecBinaryMatrix
+	void VecTextMatrixToVecBinaryMatrix();
+
+
+	imageIO()
+		: str_image_data(""),str_file_format(""),str_width(""),str_height(""),str_matrix(""), str_max_value(""),\
+			nwidth(0), nheight(0), nmax_value(0){}	//默认类构造函数
 private:
-	string strFileFormat;
-	vector<string> strComment;
-	int nWidth;
-	int nHeigth;
-	int nMaxColor;
-	vector<int> vecRow;
-	vector<vector<int>> vecColumn;
+	string str_image_data;	//整个image数据
+
+	string str_file_format;	//文件格式
+	string str_comment;		//文件注释
+	string str_width;		//文件宽度string
+	string str_height;		//文件高度string
+	string str_matrix;		//文件矩阵数据
+	string str_max_value;	//文件最大值
+
+	int nwidth;				//宽度
+	int nheight;			//高度
+	int nmax_value;			//最大值
+
+	vector<int> vec_binary_matrix;	//二进制Matrix
+	vector<int> vec_text_matrix;	//文本Matrix
+
+	vector<string> vec_comment;		//由于注释行数不知,用vector来储存.
 };
 
 #endif
